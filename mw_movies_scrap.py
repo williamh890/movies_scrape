586f4630b9a6c8a46a4d720d49bdae99bc995b7b
williamh890 <wbhorn@alaska.edu>
2017-07-07 00:05:41 -0800

    Downloads movie data from twitter
    Got through the whole list of movies, fixed  some spelling mistakes in the
    movie list itself. Lots of error and if one does occur it just moves onto
    the next  movie. given 5 search results from wikipedia to get the movie,
    otherwise moves on. Need to find a clearer way of writing the data as well
    as a printout at the that summarizes the overall  run like what movies
    failed and what movies didnt.

--- mw_movies_scrap.py
+++ mw_movies_scrap.py
@@ -24,30 +24,81 @@ def search_wikipedia(movie):
     search_results = requests.get(url, param).text.replace(u'\xa0', u' ')
     search_results = json.loads(search_results)
 
-    first_search_url = search_results[-1][0]
+    try:
+        search_results = search_results[-1]
+    except:
+        print(search_results)
+        return None
 
-    return first_search_url
+    return search_results
+
+
+
+def write_data(label, data, file):
+    line = label + ","
+    for data in data[label]:
+        line += data + ","
+    line = line[:-1] + "\n"
+    file.write(line)
+
+
+def get_movie_data(movie, file):
+    # Just do one for testing
+    movie_title = movie
+    print(movie_title)
+
+    search_results = search_wikipedia(movie_title)
+
+    if search_results is None:
+        print("No search results found")
+        return
+
+    for search_url in search_results:
+        print(search_url)
+        request = requests.get(search_url)
+        movie_soup = BeautifulSoup(request.text, "lxml")
+
+        table_data = []
+        table = movie_soup.find("table", {"class": "infobox"})
+
+        # Cant find the data table
+        if table is None:
+            print("Cant find table data...")
+            continue
+
+        for tr in table.find_all("tr"):
+            table_row = tr.text.split("\n")
+            table_row = [r for r in table_row if r != ""]
+            table_data.append(table_row)
+
+        try:
+            table_data = {r[0].strip(): r[1:] for r in table_data}
+        except:
+            print("Cant remove whitespace...")
+            continue
+        isMovie = True
+        data_labels = ["Box office", "Starring", "Running time"]
+        try:
+            data = [table_data[label] for label in data_labels]
+        except:
+            print("Error, not all labels present in {}".format(str(data_labels)))
+            continue
+        else:
+            file.write(movie_title + ",\n")
+
+        for label in data_labels:
+            print("Writing label {} to file".format(label))
+            try:
+                write_data(label, table_data, file)
+                file.write(",\n")
+            except:
+                continue
+        print("done.\n")
+        return
 
 
 if __name__ == "__main__":
-    movies_titles = read_movies_from_file("MW-Movies.dat")
-
-    # Just do one for testing
-    movie_title = movies_titles[0]
-    print(movie_title)
-
-
-    search_url = search_wikipedia(movie_title)
-    print(search_url)
-
-
-    request = requests.get(search_url)
-    movie_soup = BeautifulSoup(request.text, "html.parser")
-
-    with open("bravehear_tables.html", "w") as file:
-        for row in movie_soup.find_all("table", {"class": "infobox"}):
-            try:
-                file.write(row)
-                file.write("\n\n")
-            except:
-                pass
+    movie_titles = read_movies_from_file("MW-Movies.dat")
+    with open("movie_data.csv", "w") as file:
+        for movie in movie_titles:
+            get_movie_data(movie, file)
